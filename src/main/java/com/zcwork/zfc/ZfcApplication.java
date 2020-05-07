00c60261e1b679f9600cb357f514b79b909d3d42
zc <584787434@qq.com>
2020-05-07 09:20:52 +0800

    开始

---
+++ src/main/java/com/zcwork/zfc/ZfcApplication.java
@@ -0,0 +1,13 @@
+package com.zcwork.zfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zcwork/zfc/ZfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcwork.zfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
