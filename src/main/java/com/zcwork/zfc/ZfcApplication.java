24ec48160f231a86823c9eafae23dfc7ab0b30f4
zc <584787434@qq.com>
2020-05-08 16:41:35 +0800

    wu

--- src/main/java/com/zcwork/zfc/ZfcApplication.java
+++ src/main/java/com/zcwork/zfc/ZfcApplication.java
@@ -2,6 +2,7 @@ package com.zcwork.zfc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.annotation.Validated;
 
 @SpringBootApplication
 public class ZfcApplication {
---
+++ src/main/java/com/zcwork/zfc/pojo/Person.java
@@ -0,0 +1,20 @@
+package com.zcwork.zfc.pojo;
+
+
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+public class Person {
+
+
+
+      private String name;
+
+
+      private Integer age;
+
+
+
+
+
+}
